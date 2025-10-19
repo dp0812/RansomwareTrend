2572d64a79dfa1cb959005b3ac27d22a06f2b630
Dang Pham <tomphnd0812@gmail.com>
2025-10-19 16:22:22 -0600

    test pull

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args){
         System.out.println("This is a test file for the research project.");
+        System.out.println("Just clone to another device");
     }
 }
