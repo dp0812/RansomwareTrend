67ff110d9e414c6561d72a407673b6bec4e5429f
Dang Pham <tomphnd0812@gmail.com>
2025-11-24 07:36:52 -0700

    The express set up is now functional, and use File.separator to handle file independent of OS.

--- src/fileReader/CsvSaver.java
+++ src/fileReader/CsvSaver.java
@@ -1,5 +1,6 @@
 package fileReader;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -10,27 +11,36 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 /**
- * A Java program to download a CSV file from a URL and save it to src/datasets
- * The program assumes the target directory 'src/datasets' already exists.
+ * This class pull the data from a hardcoded site in the field FILE_URL to create a csv file called recent.csv 
+ * For the class to behave properly, the datasets directory must exists inside the src directory. 
+ * This class is designed to be system independent - the file path will work for both Linux and Windows. 
  */
 public class CsvSaver {
-
+    /** Where the info is fetch from. DO NOT fetch more than 1 time every 5 mins. */
     private static final String FILE_URL = "https://bazaar.abuse.ch/export/csv/recent";
+    /** Default csv file name. */
     private static final String FILE_NAME = "recent.csv";
-    private static final String TARGET_DIRECTORY = "src/datasets";
+    /** Default directory. */
+    private static final String TARGET_DIRECTORY = "src" + File.separator + "datasets";
 
+    /**
+     * This fetch information from a site to build a csv file for data processing.
+     * After using this method, the caller must impose a wait on the thread to clear the transient lock on the csv file before reading the file.
+     * Else, exception will be thrown.
+     * @return Name of the file created, with no directory or path attached to it. 
+     */
     public static String importCsvFromWeb() {
         try {
             Path targetDir = Paths.get(TARGET_DIRECTORY);
             Path targetFilePath = targetDir.resolve(FILE_NAME);
-            //connect.
+            //connect
             URI uriObject = new URI(FILE_URL);
             URL url = uriObject.toURL();
             IO.println("Attempting to download from: " + FILE_URL);
-            //Use Files.copy to download. 
+            //use Files.copy to download
             try (InputStream in = url.openStream()) {
                 long bytesCopied = Files.copy(in, targetFilePath, StandardCopyOption.REPLACE_EXISTING);
-                IO.println("Download successful! Copy: " + bytesCopied + " to required file.");
+                IO.println("Download successful! Copy: " + bytesCopied + " bytes to required file: " + FILE_NAME);
                 IO.println("File saved to: " + targetFilePath.toAbsolutePath());
             }
         } catch (IOException e) {
